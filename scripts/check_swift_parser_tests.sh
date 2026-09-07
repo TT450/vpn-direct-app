f2fed270afe53960f039bdb6ecb0548e4e226688
TheTochka <39283886+TT450@users.noreply.github.com>
2026-09-07 22:36:30 +0400

    test: sync Xray JSON adapter into parser package

--- scripts/check_swift_parser_tests.sh
+++ scripts/check_swift_parser_tests.sh
@@ -14,6 +14,7 @@ for file in \
   HysteriaShareLinkParser.swift \
   MieruConfigAdapter.swift \
   UniversalOutboundBuilder.swift \
+  XrayJSONAdapter.swift \
   XrayVLESSConverter.swift \
   VPNDirectCoreCapabilities+Compatibility.swift
 do
